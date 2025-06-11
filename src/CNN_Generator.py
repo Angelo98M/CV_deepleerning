f7f73ce686b93d028f0446a4224b20ebbd8b4ed0
Angelo Mavridis <Angelo.m1998@hotmail.com>
2025-06-11 14:14:45 +0200

    New maschien with restn101

--- src/CNN_Generator.py
+++ src/CNN_Generator.py
@@ -63,3 +63,36 @@ def create_new_model(image_shape):
     model.add(keras.layers.Dense(units=29,activation="softmax"))
 
     return model
+
+def create_new_model_v2(image_shape):
+    rest = keras.applications.ResNet101(
+        include_top=False,
+        weights="imagenet",
+        input_shape = image_shape,
+        name="restnet101"
+    )
+    for layer in rest.layers:
+        layer.trainable = False
+
+
+    model = keras.Sequential()
+    model.add(keras.layers.RandomFlip("horizontal_and_vertical"))
+    model.add(keras.layers.RandomRotation(0.4))
+    model.add(keras.layers.RandomZoom(0.4))
+    model.add(keras.layers.RandomContrast(0.4))
+    model.add(keras.layers.RandomBrightness(0.4))
+    model.add(keras.layers.RandomTranslation(0.4,0.3))
+    model = keras.Sequential()
+    model.add(rest)
+    #model.add(keras.layers.GlobalAvgPool2D())
+    model.add(keras.layers.Flatten())
+    model.add(keras.layers.Dense(units=512,activation="relu"))
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.Dense(units=1024,activation="relu"))
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.Dense(units=2048,activation="relu"))
+    model.add(keras.layers.Dropout(0.3))
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.Dense(units=29,activation="softmax"))
+
+    return model
