b5fb9830349ee4be9ab0593e688b0312b749cb2b
user8193 <luca_fabio.eberhardt@fh-bielefel.de>
2025-06-12 00:59:41 +0200

    list misidentified files

--- src/create_matrix.py
+++ src/create_matrix.py
@@ -1,31 +1,39 @@
 import Load_data
 import Read_Write_Model
 import numpy as np
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report, confusion_matrix, ConfusionMatrixDisplay
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 LABELS = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "del", "nohing", "space"]
 
-def create_confusion_matrix(model, dataset):
+def create_predictions(model, dataset):
     y_pred_labels = np.array([])
     y_true = np.array([])
     for x, y in dataset:
-        y_pred_labels = np.concatenate([y_pred_labels,
-                                        np.argmax(model.predict(x, verbose=0), axis=-1)])
+        y_pred_labels = np.concatenate([y_pred_labels, np.argmax(model.predict(x, verbose=0), axis=-1)])
         y_true = np.concatenate([y_true, y.numpy()])
 
-    return confusion_matrix(y_true, y_pred_labels)
+    return y_true, y_pred_labels
 
 def main():
     shape_template = (200, 200)
     model = Read_Write_Model.Load_model("./models/LT12.keras")
     dataset = Load_data.load_test_data("Data/archive2", shape_template)
-    cm = create_confusion_matrix(model, dataset)
-    sns.heatmap(cm, annot=True, fmt='d', yticklabels=LABELS, xticklabels=LABELS)
-    plt.xlabel('Predicted')
-    plt.ylabel('True')
-    plt.show()
+
+    y_true, y_pred_labels = create_predictions(model, dataset)
+    print(classification_report(y_true, y_pred_labels, target_names=LABELS))
+    # cm = confusion_matrix(y_true, y_pred_labels)
+
+    # plt.figure(figsize = (20,20))
+    # sns.heatmap(cm, annot=True, fmt='d', yticklabels=LABELS, xticklabels=LABELS)
+    # disp = ConfusionMatrixDisplay(cm, display_labels=LABELS)
+    # _, ax = plt.subplots(figsize=(10,10))
+    # disp.plot(ax=ax)
+    # plt.xlabel('Predicted')
+    # plt.ylabel('True')
+    # plt.savefig("confusion_matrix.png")
+    # plt.show()
 
 if __name__ == "__main__":
     main()
---
+++ src/get_misidentified_files.py
@@ -0,0 +1,35 @@
+import Read_Write_Model
+import keras
+import numpy as np
+from os import listdir
+from os.path import isdir, join
+
+LABELS = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "del", "nohing", "space"]
+
+def test_image_identification(path, model, label):
+    shape_template = (200, 200)
+    #Load single image
+    img = keras.utils.load_img(path, target_size=shape_template)
+    #Convert image to numpy array
+    x = keras.utils.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    #Predition
+    pred = model.predict(x, verbose=0)
+    #Representation of the predition
+    classes = np.argmax(pred, axis=1)
+    if label == LABELS[classes[0]]: return
+    print("(", path, ")\nResult: ", LABELS[classes[0]])
+
+def main():
+    model = Read_Write_Model.Load_model("./models/LT12.keras")
+
+    for dir in listdir("./Data/archive2/test/"):
+        dir_path = join("./Data/archive2/test/", dir)
+        if not isdir(dir_path): continue
+        for file in listdir(dir_path):
+            file_path = join(dir_path, file)
+            if isdir(file_path): continue
+            test_image_identification(file_path, model, dir)
+
+if __name__ == "__main__":
+    main()
